34538815a07922f2cbee4fcb40fa429ca81cd812
Jeffrey <netteyjeff@gmail.com>
2023-07-18 17:00:30 +0000

    Todo list Features update

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 import { NewTodoForm } from "./NewTodoForm";
 import { TodoList } from "./TodoList";
 
 export default function App() {
-  const [todos, setTodos] = useState([""]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(title) {
     setTodos((currentTodos) => {
       return [
         ...currentTodos,
-        { id: crypto.randomUUID(), title: newItem, completed: false },
+        { id: crypto.randomUUID(), title, completed: false },
       ];
     });
   }
@@ -36,7 +45,7 @@ export default function App() {
     <>
       <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   );
 }
--- vite-react-app/src/TodoItem.jsx
+++ vite-react-app/src/TodoItem.jsx
@@ -1,18 +1,15 @@
-export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+export function TodoItem(completed, id, title, toggleTodo, deleteTodo) {
   return (
     <li>
       <label>
         <input
           type="checkbox"
           checked={completed}
-          //   onChange={(e) => toggleTodo(id, e.target.checked)}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
         />
         {title}
       </label>
-      <button
-        // onClick={() => deleteTodo(id)}
-        className="btn btn-danger"
-      >
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
         Delete
       </button>
     </li>
